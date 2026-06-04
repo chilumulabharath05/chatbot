d898fdec342a2b687fd016082163466a8abe5734
Chilumula Bharath <chilumulabharath7@gmail.com>
2026-06-05 02:59:03 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: { 
+    'Content-Type': 'application/json' 
+  }
 })
 
 api.interceptors.request.use(config => {
